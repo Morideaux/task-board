bb3f6f2566ccfb565b45cb801f0b6e9ae8b89747
Meish <Morideaux7@gmail.com>
2024-08-15 23:11:36 -0700

    Added task functions as well as conditional formating to the script.js file. now new task should be ABLE to be color coded according to distace to due dates using the classes that are appended by these functions. the css still needs to be updated to reflect this.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,63 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const dayjs = require('dayjs')
+var isSameOrAfter = require("dayjs/plugin/isSameOrAfter");
+dayjs.extend(isSameOrAfter);
+var isBetween = require("dayjs/plugin/isBetween");
+dayjs.extend(isBetween);
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {}
+function generateTaskId() {
+  if (nextId == null) {
+    nextId = 1;
+  } else {
+    nextId++;
+  }
+
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+  return nextId;
+}
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {}
+function createTaskCard(task) {
+  const taskCard = $("<div>").addClass().attr("obj-task-id", task.id);
+  cardHeader = $("<div>").addClass("card-header").text(task.title);
+  cardBody = $("<div>");
+  cardDescription = $("<p>")
+    .addClass(card - text)
+    .text(task.description);
+  cardDueDate = $("<p>").addClass("card-text").text(task.dueDate);
+  cardDeleteBtn = $("<button>");
+  .addClass('btn delete-btn')
+  .text('Delete')
+  .attr("obj-task-id", task.id);
+  cardDeleteBtn.on('click', handleDeleteTask)
+
+  // Conditional formating
+
+  switch (task.dueDate && task.status !== 'done') 
+  {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    case: (now.isSameOrAfter(taskDueDate, 'day')):
+        taskCard.addClass('late-task')
+        break;
+  
+    case: (now.isBetween((dayjs().subtract(15, taskDueDate)), taskDueDate, 'DD/MM/YYYY'));
+        taskCard.addClass('approch-task')
+        break;
+
+    default: (now.isbefore((dayjs().subtract(15, taskDueDate), 'DD/MM/YYYY')));
+        taskCard.addClass('safe-task');
+        break;
+  }
+
+  cardBody,append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  return taskCard;
+}
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {}
